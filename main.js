64a22dc46d86b541adb52a7dd1b1309f934647ec
Hamzat mujeeb <hamzatmujeeb586@gmail.com>
2023-05-29 17:12:44 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+var signupForm = document.getElementById('signup-form');
+signupForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var name = document.getElementById('name').value;
+  var address = document.getElementById('address').value;
+  var dbirth = document.getElementById('dbirth').value;
+  var email = document.getElementById('signup-email').value;
+  var password = document.getElementById('signup-password').value;
+
+  // Perform desired functionality here, such as making an API request or storing the user data//
+
+
+  // Prompt before starting login process
+  var confirmSignup = confirm("Are you sure you're not an existing user?");
+  // Example success message//
+  alert('Sign-up successful!');
+
+  // Clear form fields
+  signupForm.reset()
+});
+
+var loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Prompt before starting login process
+  var confirmLogin = confirm("Are you sure you want to log in?");
+
+  if (confirmLogin) {
+    var email = document.getElementById('login-email').value;
+    var password = document.getElementById('login-password').value;
+
+    // Perform login functionality here, such as making an API request or validating the user credentials
+
+    // Example success message//
+    console.log('Login successful!');
+
+    // Clear form fields
+    loginForm.reset();
+  } else {
+    console.log('Login canceled.');
+  }
+});
